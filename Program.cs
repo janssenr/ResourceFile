d0009253faabdf3f36e281c926d89ea7004215e4
Rob Janssen <rob.janssen@upcmail.nl>
2024-03-14 09:00:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string xslt = SF.R4_3.OUT.Internal.ResourceFile.Product;
+            Console.WriteLine(xslt);
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
